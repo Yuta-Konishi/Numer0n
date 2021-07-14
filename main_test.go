ed69291ed002501437883ce1533d53ae740841fd
Yuta-Konishi <86331635+Yuta-Konishi@users.noreply.github.com>
2021-07-14 12:20:58 +0900

    modify a test file

--- main_test.go
+++ main_test.go
@@ -1,10 +1,41 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"strings"
+)
 
 func Testgenerate_num01(t *testing.T) {
 	num := generate_num(1)
-	if (num - 10) >= 0 {
+	num = strings.Split(num, "")
+	length := len(num)
+	if length != 1 {
 		t.Error("Test01 is failed")
+	}
+}
+
+func Testgenerate_num02(t *testing.T) {
+	num := generate_num(2)
+	num = strings.Split(num, "")
+	length := len(num)
+	if length != 2 {
+		t.Error("Test02 is failed")
+	}else if num[0] == num[1] {
+		t.Error("Test02 is failed")
+	}
+}
+
+func Testgenerate_num03(t *testing.T) {
+	num := generate_num(3)
+	num = strings.Split(num, "")
+	length := len(num)
+	if length != 3 {
+		t.Error("Test01 is failed")
+	}else if num[0] == num[1] {
+		t.Error("Test03 is failed")
+	}else if num[1] == num[2] {
+		t.Error("Test03 is failed")
+	}else if num[2] == num[0] {
+		t.Error("Test03 is failed")
 	}
 }
